0cfd8ea2158c8a8841feaabd6a81abc60e626a91
limule <lego.lecture@gmail.com>
2026-06-25 02:24:57 +0200

    recreate keepAlive clean

--- KeepAlive.js
+++ KeepAlive.js
@@ -1,10 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Bot online");
+  res.writeHead(200);
+  res.end("Bot online");
 });
 
 server.listen(process.env.PORT || 3000, () => {
-    console.log("KeepAlive actif");
+  console.log("KeepAlive actif");
 });
